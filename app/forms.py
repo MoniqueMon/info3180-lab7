8df129d0825e6c6da073798cd62fcd684ac400a2
Monique Moncrieffe <monique.moncrieffe02@mymona.uwi.edu>
2018-04-19 14:13:23 +0000

    upload view

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired 
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import TextAreaField
 
+class UploadForm(FlaskForm):
+    dscp = TextAreaField('description', validators=[DataRequired()])
+    iamg = FileField('photo', validators=[FileRequired(), FileAllowed(['jpg', 'jpeg', 'png'],  'Images only!')])
